f78f6044c37b643dd45de95b85e227388f41b561
Nat Ravenhill <natalie.ravenhill@googlemail.com>
2018-02-17 23:31:49 +0000

    Added API call for RandomQuoteGenerator

--- RandomQuoteGenerator/index.js
+++ RandomQuoteGenerator/index.js
@@ -1,5 +1,4 @@
-var dest = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=?';
-
+var dest="https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?"
 $(document).ready(function() {
     $("#newQuote").on("click", function(){
       var htmlString = "test";
@@ -7,7 +6,8 @@ $(document).ready(function() {
       
       $.getJSON(dest, function(json){
         console.log(json);
-         $(".quote").html(json[0].content);
+         $(".quote").html(json.quoteText);
+         $(".quoteAuthor").html(json.quoteAuthor);
         
       });
     });
