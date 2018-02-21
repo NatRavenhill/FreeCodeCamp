facef69cc5aab76edb80fbae33816e93fe6c120d
Nat Ravenhill <natalie.ravenhill@googlemail.com>
2018-02-21 21:40:43 +0000

    Added tweet button to Random Quote Generator

--- RandomQuoteGenerator/index.js
+++ RandomQuoteGenerator/index.js
@@ -1,14 +1,26 @@
 var dest="https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?"
 $(document).ready(function() {
-    $("#newQuote").on("click", function(){
-      var htmlString = "test";
-
-      
-      $.getJSON(dest, function(json){
-        console.log(json);
-         $(".quote").html(json.quoteText);
-         $(".quoteAuthor").html(json.quoteAuthor);
-        
-      });
+  
+  $("#newQuote").on("click", function(){
+    var htmlString = "test";      
+    $.getJSON(dest, function(json){
+      $(".quote").text(json.quoteText);
+      $(".quoteAuthor").text(json.quoteAuthor);
     });
+  });
+
+  $("#tweetButton").on("click", function(){
+    var quoteText = $(".quote").html();
+    if(quoteText)
+    {
+      var link = 'https://twitter.com/intent/tweet?text='
+      var tweetText = encodeURIComponent( 
+        quoteText
+        + ' - '
+        + $(".quoteAuthor").html()
+        + ' #RandomQuoteGenerator');
+        window.open(link+tweetText);
+    }
+  }); 
+
 });
