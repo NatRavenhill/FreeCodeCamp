41d907e2ee82890be5c8c8f03fb764b25d5aa5ff
NatRavenhill <natalie.ravenhill@googlemail.com>
2018-05-17 17:26:40 +0100

    Fixed API call and added list of results

--- WikipediaViewer/index.js
+++ WikipediaViewer/index.js
@@ -7,13 +7,31 @@ $(document).ready(function(){
 
     $("#search").click(function(){
         var searchTerm = $("input[id=searchTerm]").val();
-        alert("am searching for " + searchTerm + "!");
 
-        var link = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm;
+        var link = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + searchTerm;
+        $.ajax(link, {
+            dataType: "json",
+            data: {
+              origin: "*"
+            },
+            type: "GET",
+            success: function(data) {
+                var titles = data[1];
+                var summaries = data[2];
+                var links = data[3];
+                for(var i = 1; i < titles.length; i++){
+                    createListItem(titles[i], summaries[i], links[i]);
+                }
 
-        $.getJSON(link, function(json){
-            $("#result").text(json);
-          });
+            }
+        });
+
     });
 });
+
+createListItem = function(title, summary, link){   
+    var item = '<li> <div class="card" <div class="card-body"><h4 class="card-title"><a href="' +
+    link + '">' + title + '</a></h4><p>' + summary + '</p></div></div></li>'; 
+    $("#results").append(item);
+}
 
