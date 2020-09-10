93fd46798b7f1df1266c241b81994a70f6090421
NatRavenhill <natalie.ravenhill@googlemail.com>
2020-09-10 21:39:00 +0100

    WikipediaViewer:Use new api query for descriptions

--- WikipediaViewer/index.js
+++ WikipediaViewer/index.js
@@ -10,7 +10,10 @@ $(document).ready(function(){
         });
 });
 
-createListItem = function(title, summary, link){   
+createListItem = function(result){ 
+    var title = result.title;
+    var summary = result.snippet;
+    var link =   "https://en.wikipedia.org/wiki/" + result.title;
     var item = '<li> <div class="card" <div class="card-body"><h4 class="card-title"><a href="' +
     link + '">' + title + '</a></h4><p>' + summary + '</p></div></div></li>'; 
     $("#results").append(item);
@@ -19,7 +22,7 @@ createListItem = function(title, summary, link){
 search = function(){
     var searchTerm = $("input[id=searchTerm]").val();
 
-    var link = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + searchTerm;
+    var link = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" + searchTerm;
     $.ajax(link, {
         dataType: "json",
         data: {
@@ -27,12 +30,10 @@ search = function(){
         },
         type: "GET",
         success: function(data) {
-            var titles = data[1];
-            var summaries = data[2];
-            var links = data[3];
+            var searchResults = data.query.search;
             $("#results").empty();
-            for(var i = 1; i < titles.length; i++){
-                createListItem(titles[i], summaries[i], links[i]);
+            for(var i = 1; i < searchResults.length; i++){
+                createListItem(searchResults[i]);
             }
 
         }
